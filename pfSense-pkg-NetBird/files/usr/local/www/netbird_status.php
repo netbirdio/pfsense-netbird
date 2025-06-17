9e56f408c4c21d25d74f2cd92fa2ebff4b9d45a0
bcmmbaga <bethuelmbaga12@gmail.com>
2025-06-17 17:48:12 +0300

    init package

---
+++ pfSense-pkg-NetBird/files/usr/local/www/netbird_status.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title"><?=gettext("NetBird Status")?>
+            </h2>
+        </div>
+<!--        <div class="panel-body">-->
+<!--            <p>TOStatus</p>-->
+<!--        </div>-->
+    </div>
+
+<?php include("foot.inc");
+
+
