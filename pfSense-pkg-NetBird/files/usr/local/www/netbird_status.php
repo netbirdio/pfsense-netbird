69bde186adf7ca4f9e86633d200c28cdb95ba2a2
bcmmbaga <bethuelmbaga12@gmail.com>
2025-06-18 23:55:23 +0300

    fix url path reference

--- pfSense-pkg-NetBird/files/usr/local/www/netbird_status.php
+++ pfSense-pkg-NetBird/files/usr/local/www/netbird_status.php
@@ -1,6 +1,19 @@
 <?php
 
+require_once('guiconfig.inc');
+require_once('util.inc');
+
+
+$tab_array = [];
+$tab_array[] = [gettext('Authentication'), false, 'pkg_edit.php?xml=netbird/netbird_auth.xml'];
+$tab_array[] = [gettext('Settings'), false, 'pkg_edit.php?xml=netbird.xml'];
+$tab_array[] = [gettext('Status'), true, '/netbird_status.php'];
+
+include('head.inc');
+
+display_top_tabs($tab_array);
 ?>
+
     <div class="panel panel-default">
         <div class="panel-heading">
             <h2 class="panel-title"><?=gettext("NetBird Status")?>
@@ -11,6 +24,7 @@
 <!--        </div>-->
     </div>
 
-<?php include("foot.inc");
+<?php
+include('foot.inc');
 
 
